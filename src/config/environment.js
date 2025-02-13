8866a58c6e22994dde0e5a271563d4a1e19a8d27
Nhat Ngoc Minh Dinh <67326455+minhat0601@users.noreply.github.com>
2025-02-13 13:37:39 +0700

    update

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,3 +3,14 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import 'dotenv/config'
+
+export const env = {
+    APP_HOST: process.env.APP_HOST,
+    APP_PORT: process.env.APP_PORT,
+
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_USERNAME: process.env.DB_USERNAME,
+    MONGODB_URI: process.env.MONGODB_URI
+}
