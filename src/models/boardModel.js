5bdac8362b0c469ed5ea67a55cee7ee88b0d5233
Nhat Ngoc Minh Dinh <67326455+minhat0601@users.noreply.github.com>
2025-02-14 12:05:40 +0700

    fix

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -3,13 +3,25 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+import Joi from "joi"
+// Import pattern để check kiểu dữ liệu ObjectId Dành cho mongoDB
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
-const createNew = async () => {
+// Define colletion (name & schema)
 
-}
-
-
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+    title: Joi.string().min(3).max(50).required().trim().strict(),
+    slug: Joi.string().min(3).required().trim().strict(),
+    decription: Joi.string().min(3).max(255).required().trim().strict(),
+    columnOrderIds: Joi.array().items(
+        Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    ).default([]),
+    createdAt: Joi.date().timestamp('javascript').default(Date.now),
+    updatedAt: Joi.date().timestamp('javascript').default(null),
+    _destroy: Joi.boolean().default(false)
+})
 
 export const boardModel = {
-    createNew
+    BOARD_COLLECTION_NAME, BOARD_COLLECTION_SCHEMA
 }
